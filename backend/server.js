809e422744872c3cc51a60164cf43d5cd28c0b05
Riddhi0809 <143692390+Riddhi0809@users.noreply.github.com>
2026-01-17 09:21:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,5 +51,8 @@ app.post('/api/payment/orders', async (req, res) => {
 });
 
 /* -------------------- Server -------------------- */
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+/*const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));*/
+
+module.exports = app;
+
